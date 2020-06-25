df2c57c8a2c1d6f7d23a5cb19672d9eba93532e0
kings10-hiroki <h61147366@gmail.com>
2020-06-25 13:52:03 +0900

    Fixed authController-2

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,13 +16,23 @@ const signToken = id => {
 const createSendToken = (user, statusCode, req, res) => {
     const token = signToken(user._id);
 
-    res.cookie('jwt', token, {
+    // res.cookie('jwt', token, {
+    //     expires: new Date(
+    //         Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    //     ),
+    //     httpOnly: true,
+    //     secure: req.secure || req.headers['x-forwarded-photo'] === 'https'
+    // });
+
+    const cookieOptions = {
         expires: new Date(
             Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true,
-        secure: req.secure || req.headers['x-forwarded-photo'] === 'https'
-    });
+        httpOnly: true
+    };
+    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+    res.cookie('jwt', token, cookieOptions);
 
     user.password = undefined;
 
